2f471b1007f3e9dc0bd975ad280287056e6b8ebd
Keriteal <keriteal985+git@gmail.com>
2020-12-25 19:12:12 +0800

    Enhance PackedResponse

--- src/main/java/com/my/workmanagement/payload/PackedResponse.java
+++ src/main/java/com/my/workmanagement/payload/PackedResponse.java
@@ -1,13 +1,41 @@
 package com.my.workmanagement.payload;
 
+import java.time.LocalDateTime;
+
 public class PackedResponse<T> {
     private final T data;
+    private final String message;
+    private final ResponseResult result;
+    private final LocalDateTime timestamp;
 
-    public PackedResponse(T data) {
+    private PackedResponse(T data, String message, ResponseResult result) {
         this.data = data;
+        this.message = message;
+        this.result = result;
+        timestamp = LocalDateTime.now();
+    }
+
+    public static <T> PackedResponse<T> success(T data, String message) {
+        return new PackedResponse<>(data, message, ResponseResult.SUCCESS);
+    }
+
+    public static <T> PackedResponse<T> failed(T data, String message) {
+        return new PackedResponse<>(data, message, ResponseResult.FAILED);
     }
 
     public T getData() {
         return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ResponseResult getResult() {
+        return result;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 }
