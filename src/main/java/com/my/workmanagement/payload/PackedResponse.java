92a80b355952dddee1f29bd98ae215029d01a63b
Keriteal <keriteal985+git@gmail.com>
2020-12-28 15:02:24 +0800

    Extend PackedResponse

--- src/main/java/com/my/workmanagement/payload/PackedResponse.java
+++ src/main/java/com/my/workmanagement/payload/PackedResponse.java
@@ -1,6 +1,7 @@
 package com.my.workmanagement.payload;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,16 @@ public final class PackedResponse<T> {
 
     public static <T> ResponseEntity<PackedResponse<T>> badRequest(T data, String message) {
         return ResponseEntity.badRequest().body(new PackedResponse<>(data, message, ResponseResult.FAILED));
+    }
+
+    public static <T> ResponseEntity<PackedResponse<T>> notFound(T data, String message) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new PackedResponse<>(data, message, ResponseResult.FAILED));
+    }
+
+    public static <T> ResponseEntity<PackedResponse<T>> failure(T data, String message, HttpStatus code) {
+        return ResponseEntity.status(code)
+                .body(new PackedResponse<>(data, message, ResponseResult.FAILED));
     }
 
     public static <T> ResponseEntity<PackedResponse<T>> success(T data, String message) {
