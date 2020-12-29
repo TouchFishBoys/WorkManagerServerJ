d588f85425128a8370bafd0847e856d41427b8af
zhouchong0910du@163.com <zhouchong0910du@163.com>
2020-12-29 16:15:40 +0800

    add NormalWorkRepository

--- src/main/java/com/my/workmanagement/repository/NormalWorkRepository.java
+++ src/main/java/com/my/workmanagement/repository/NormalWorkRepository.java
@@ -1,9 +1,11 @@
 package com.my.workmanagement.repository;
 
 import com.my.workmanagement.entity.NormalWorkDO;
+import com.my.workmanagement.entity.StudentDO;
 import com.my.workmanagement.entity.TopicDO;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NormalWorkRepository extends CrudRepository<NormalWorkDO,Integer> {
 
+    Integer countAllByStudent(StudentDO studentDO);
 }
--- src/main/java/com/my/workmanagement/service/StudentServiceImpl.java
+++ src/main/java/com/my/workmanagement/service/StudentServiceImpl.java
@@ -6,11 +6,10 @@ import com.my.workmanagement.exception.IdNotFoundException;
 import com.my.workmanagement.model.bo.CourseInfoBO;
 import com.my.workmanagement.model.bo.StudentInfoBO;
 import com.my.workmanagement.payload.response.student.StudentInfoResponse;
-import com.my.workmanagement.repository.CourseRepository;
-import com.my.workmanagement.repository.CourseSelectionRepository;
-import com.my.workmanagement.repository.StudentRepository;
+import com.my.workmanagement.repository.*;
 import com.my.workmanagement.service.interfaces.AuthService;
 import com.my.workmanagement.service.interfaces.StudentService;
+import org.hibernate.hql.internal.ast.util.TokenPrinters;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,10 +21,19 @@ public class StudentServiceImpl implements StudentService {
 
     private StudentRepository studentRepository;
     private CourseSelectionRepository courseSelectionRepository;
+    private NormalWorkRepository normalWorkRepository;
+    private TopicRepository topicRepository;
 
-    StudentServiceImpl(StudentRepository studentRepository,CourseSelectionRepository courseSelectionRepository) {
+    StudentServiceImpl(
+            StudentRepository studentRepository
+            , CourseSelectionRepository courseSelectionRepository
+            , NormalWorkRepository normalWorkRepository
+            , TopicRepository topicRepository
+    ) {
         this.studentRepository = studentRepository;
-        this.courseSelectionRepository=courseSelectionRepository;
+        this.courseSelectionRepository = courseSelectionRepository;
+        this.normalWorkRepository = normalWorkRepository;
+        this.topicRepository = topicRepository;
     }
 
     @Override
@@ -41,7 +49,7 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public StudentInfoBO getStudentInfo(Integer studentId) throws IdNotFoundException {
         StudentDO studentDO = studentRepository.findByStudentId(studentId);
-        StudentInfoBO studentInfoBO=new StudentInfoBO();
+        StudentInfoBO studentInfoBO = new StudentInfoBO();
         studentInfoBO.setStudentNum(studentDO.getStudentNum());
         studentInfoBO.setStudentName(studentDO.getStudentName());
         studentInfoBO.setStudentId(studentId);
@@ -50,18 +58,17 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    public List<CourseInfoBO> getCourseSelectionInfo(Integer studentId) throws IdNotFoundException
-    {
-        List<CourseInfoBO> list=null;
+    public List<CourseInfoBO> getCourseSelectionInfo(Integer studentId) throws IdNotFoundException {
+        List<CourseInfoBO> list = null;
         StudentDO studentDO = studentRepository.findByStudentId(studentId);
-        List<CourseDO> courseDOS=courseSelectionRepository.findAllByStudent(studentDO);
-        for(int i=0;i<courseDOS.size();i++){
+        List<CourseDO> courseDOS = courseSelectionRepository.findAllByStudent(studentDO);
+        for (int i = 0; i < courseDOS.size(); i++) {
             list.add(CourseInfoBO.CourseInfoBOBuilder.aCourseInfoBOBuilder()
                     .withCourseName(courseDOS.get(i).getCourseName())
                     .withCourseId(courseDOS.get(i).getCourseId())
                     .withCourseTeacherName(courseDOS.get(i).getTeacher().getTeacherName())
-                    //.withFinishCount()
-                    //.withTotalCount()
+                    .withFinishCount(normalWorkRepository.countAllByStudent(studentDO))
+                    .withTotalCount(topicRepository.countAllByCourseId(courseDOS.get(i).getCourseId()))
                     .build()
             );
         }
