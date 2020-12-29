310aa0501d84021d93f8dffedb399e134ff603f1
Keriteal <keriteal985+git@gmail.com>
2020-12-29 22:26:54 +0800

    Update FileStorageService

--- src/main/java/com/my/workmanagement/service/FileStorageServiceImpl.java
+++ src/main/java/com/my/workmanagement/service/FileStorageServiceImpl.java
@@ -1,11 +1,23 @@
 package com.my.workmanagement.service;
 
+import com.my.workmanagement.exception.StorageFileNotFoundException;
 import com.my.workmanagement.service.interfaces.FileStorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 @Service
 public class FileStorageServiceImpl implements FileStorageService {
+    @Value("${workmanager.storage.root-directory:/storage}")
+    private String storageRoot;
+
     @Override
     public boolean store(MultipartFile file) {
 
@@ -15,5 +27,18 @@ public class FileStorageServiceImpl implements FileStorageService {
     @Override
     public boolean load() {
         return false;
+    }
+
+    @Override
+    public Resource loadAsResource(String path) throws StorageFileNotFoundException {
+        File file = new File(storageRoot + path);
+        if (!file.exists() || !file.isFile()) {
+            throw new StorageFileNotFoundException();
+        }
+        try {
+            return new InputStreamResource(new FileInputStream(file));
+        } catch (FileNotFoundException ex) {
+            throw new StorageFileNotFoundException();
+        }
     }
 }
